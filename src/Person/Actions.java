544e29fbb3d99a649eef896256bf9d3f69f50946
brolywildfire <rafaelwildfire@gmail.com>
2023-12-27 20:16:26 -0300

    feat: fight actions

---
+++ src/Person/Actions.java
@@ -0,0 +1,16 @@
+package Person;
+
+public interface Actions {
+    public void attackHighKick();
+    public void attackLowKick();
+    public void attackJab();
+    public void attackUppercut();
+    public void attackNearHeadChoke();
+    public void attackArmTriangle();
+    public void attackLegLock();
+    public void attackTakedown();
+    public void defendTakedown();
+    public void defendHandsAttack();
+    public void defendLegsAttack();
+    public void specialStrike();
+}
